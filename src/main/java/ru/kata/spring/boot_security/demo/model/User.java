9ab82dbd4bf2218d9551eb62cf140c1f7cd14b38
Jarikjarik <jarikkickass@mail.ru>
2025-12-19 16:10:29 +0500

    Fix bugs

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
     private String lastName;
     private String email;
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
