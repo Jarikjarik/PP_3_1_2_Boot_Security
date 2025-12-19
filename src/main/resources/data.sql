caa8d0d89f3785577d240d8847cdee55e90c29bf
Jarikjarik <jarikkickass@mail.ru>
2025-12-19 15:01:13 +0500

    Fix bugs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,11 +4,11 @@ INSERT INTO roles (id, name) VALUES (2, 'ROLE_ADMIN');
 
 -- Пользователи
 INSERT INTO users (id, username, password, name, last_name, email)
-VALUES (1, 'user', '$2a$10$ZrO9xIHhjzCcBjiabPxf9OW0lJJBoO.cCDE9KfltkOZKk9P8KOkb2',
+VALUES (1, 'user',  '$2a$10$jvDgY4cJ0iqw8gXduysnoOja7MKM6YliA5oQYnxNJgEIg0Vw4Siee',
         'UserName', 'UserLast', 'user@mail.com');
 
 INSERT INTO users (id, username, password, name, last_name, email)
-VALUES (2, 'admin', '$2a$10$uBtRGuL4ExJIBrMgGZkM9uGnpNvtw0ZoUcOPLOQlbb9aix4.0Gn2K',
+VALUES (2, 'admin', '$2a$10$gpHnRyU9.RYlkDCBKQIocOklviPrtdSHFjtEaDx8oX1cM8K6QLZ7O',
         'AdminName', 'AdminLast', 'admin@mail.com');
 
 -- Связи user_roles
